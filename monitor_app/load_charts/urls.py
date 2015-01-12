96f38aaf625629004d5b7dbaf5ee2f646aee93e6
Xavier <xajouppe@gmail.com>
2015-01-12 09:11:40 +0100

    AJAX implementation

--- monitor_app/load_charts/urls.py
+++ monitor_app/load_charts/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('load_charts.views',
     url(r'^$', 'index'),
+    url(r'^test-get/$', 'test_get'),
 )
--- monitor_app/load_charts/views.py
+++ monitor_app/load_charts/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import subprocess
+import re
+import json
 
 # Create your views here.
 def index(request):
-	r = subprocess.check_output(["uptime"])
+	raw_file=open('./load_charts/static/load_charts/uptime_log', 'w')
+	raw_file.write('')
+	raw_file.close
 	return render(request, 'load_charts/index.html', locals())
+
+def test_get(request):
+	data = update_log()
+	return HttpResponse(data)
+
+
+def update_log():
+	r = subprocess.check_output(["uptime"])
+	hour = r[:5]
+	load_averages = re.split("load averages: ", r)
+	load_avg_last_min = re.split(" ",load_averages[1])[0]
+
+	raw_file=open('./load_charts/static/load_charts/uptime_log', 'a+')
+
+
+	string_to_add = ',{"hour":"'+hour+'","value":'+load_avg_last_min.replace(',', '.')+'}\n'
+
+	raw_file.write(string_to_add)
+	raw_file.seek(0)
+	data = "["+raw_file.read()[1:]+"]"
+	raw_file.close
+	return data
