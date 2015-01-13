ee63d6e8ffd0a0be587ea1f3f80bb826b37a7273
Xavier <xajouppe@gmail.com>
2015-01-13 20:21:52 +0100

    fix on date use in view.py

--- monitor_app/load_charts/views.py
+++ monitor_app/load_charts/views.py
@@ -4,10 +4,8 @@ import subprocess
 import re
 import json
 from datetime import timedelta
-from time import mktime, strftime, strptime, localtime
 import time
 import datetime
-import ast
 
 UPTIME_VALUES = []
 ALERT = False
@@ -15,52 +13,62 @@ ALERT = False
 # Create your views here.
 def index(request):
 	global UPTIME_VALUES
+	global ALERT
 	del UPTIME_VALUES[:]
 	ALERT = False
-	time = strftime("%a, %d %b %Y %H:%M:%S", localtime())
+	time = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S")
 	return render(request, 'load_charts/index.html', locals())
 
 def charts_get(request):
 	global UPTIME_VALUES
 	time_interval = datetime.datetime.now() - timedelta(minutes=10)
 	for pos, val in enumerate(UPTIME_VALUES):
-		val_date = datetime.datetime.strptime(val["date"], "%Y-%m-%d %H:%M:%S")
-		if time_interval > val_date or pos > 60:
+		if time_interval > val["date"] or pos > 60:
 			UPTIME_VALUES.pop(pos)
 	UPTIME_VALUES.insert(0,get_uptime_vals())
-	data = json.dumps(UPTIME_VALUES)
-	return HttpResponse(data)
+	print len(UPTIME_VALUES)
+	return HttpResponse(convert_json(UPTIME_VALUES))
+
 
 def check_alerts(request):
 	global ALERT
 	threshold = float(request.GET.get('threshold', ''))
 	time_interval = datetime.datetime.now() - timedelta(minutes=2)
 	sum_avg = 0
-	data = -1
 	nb = 0
+	data = []
+	data.append({"alert":-1})
 	for pos, val in enumerate(UPTIME_VALUES):
-		val_date = datetime.datetime.strptime(val["date"], "%Y-%m-%d %H:%M:%S")
-		if val_date > time_interval:
+		if val["date"] > time_interval:
 			sum_avg += val["value"]
 			nb += 1
 	if nb:
 		avg = sum_avg / nb
+		data.append({"value":avg})
 		if avg > threshold:
 			if not ALERT:
 				ALERT = True
-				data = 1
-			if ALERT:
-				data = -1
+				data.append({"alert":1})
 		elif ALERT and avg < threshold:
 			ALERT=False
-			data = 0
-	return HttpResponse(data)
+			data.append({"alert":0})
+	return HttpResponse(json.dumps(data))
+
+def convert_json(values):
+	temp_values = []
+	for val in UPTIME_VALUES:
+		temp_values.append(entry(val["date"].strftime("%Y-%m-%d %H:%M:%S"),val["value"]))
+	return json.dumps(temp_values)
 
 def get_uptime_vals():
 	r = subprocess.check_output(["uptime"])
 	load_averages = re.split("load averages: ", r)
 	load_avg_last_min = re.split(" ",load_averages[1])[0].replace(',', '.')
+
+	return entry(datetime.datetime.now(), float(load_avg_last_min))
+
+def entry(date, value):
 	new_entry = {}
-	new_entry["date"] = strftime("%Y-%m-%d %H:%M:%S", localtime())
-	new_entry["value"] = float(load_avg_last_min)
+	new_entry["date"] = date
+	new_entry["value"] = value
 	return new_entry
