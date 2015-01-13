c3c8a812e53ca91bdf5494dc0520f8db60b760e2
Xavier <xajouppe@gmail.com>
2015-01-13 15:40:59 +0100

    first alert system implementation

--- monitor_app/load_charts/urls.py
+++ monitor_app/load_charts/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('load_charts.views',
     url(r'^$', 'index'),
     url(r'^charts-get/$', 'charts_get'),
+    url(r'^check-alerts/$', 'check_alerts'),
 )
--- monitor_app/load_charts/views.py
+++ monitor_app/load_charts/views.py
@@ -3,16 +3,19 @@ from django.http import HttpResponse
 import subprocess
 import re
 import json
-from datetime import datetime, timedelta
+from datetime import timedelta
 from time import mktime, strftime, strptime, localtime
 import time
+import datetime
 import ast
 
 UPTIME_VALUES = []
+ALERT = False
+SEUIL = 1.50
 
 # Create your views here.
 def index(request):
-	UPTIME_VALUES = []
+	del UPTIME_VALUES[:]
 	time = strftime("%a, %d %b %Y %H:%M:%S", localtime())
 	return render(request, 'load_charts/index.html', locals())
 
@@ -20,26 +23,43 @@ def charts_get(request):
 	data = update_log()
 	return HttpResponse(data)
 
+def check_alerts(request):
+	time_interval = datetime.datetime.now() - timedelta(minutes=2)
+	data = ''
+	sum_avg = 0
+	nb = 0
+	for pos, val in enumerate(UPTIME_VALUES):
+		val_date = datetime.datetime.strptime("%Y-%m-%d %H:%M:%S", val["date"])
+		if val_date > time_interval:
+			sum_avg += val["value"]
+			nb += 1
+	if nb:
+		avg = sum_avg / nb
+		if avg > SEUIL:
+			if not ALERT:
+				ALERT = True
+				data = "alert"
+		else:
+			if ALERT:
+				ALERT=False
+				data = "finalert"
+	return HttpResponse(data)
+
 
 def update_log():
-	time_interval = datetime.now() - timedelta(minutes=10)
+	time_interval = datetime.datetime.now() - timedelta(minutes=10)
 	for pos, val in enumerate(UPTIME_VALUES):
-		u = datetime.fromtimestamp(mktime(time.strptime(val["date"], "%Y-%m-%d %H:%M:%S")))
-		if time_interval > u or pos > 44:
+		val_date = datetime.datetime.strptime("%Y-%m-%d %H:%M:%S", val["date"])
+		if time_interval > val_date or pos > 60:
 			UPTIME_VALUES.pop(pos)
 	UPTIME_VALUES.insert(0,get_uptime_vals())
 	return json.dumps(UPTIME_VALUES)
-
-def empty_file(path):
-	open(path, 'w').close()
 
 def get_uptime_vals():
 	r = subprocess.check_output(["uptime"])
 	load_averages = re.split("load averages: ", r)
 	load_avg_last_min = re.split(" ",load_averages[1])[0].replace(',', '.')
-
 	new_entry = {}
 	new_entry["date"] = strftime("%Y-%m-%d %H:%M:%S", localtime())
 	new_entry["value"] = float(load_avg_last_min)
-
 	return new_entry
