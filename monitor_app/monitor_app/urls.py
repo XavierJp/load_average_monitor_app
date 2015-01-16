fb2d2733d20cdd3624f59478574c343002206253
Xavier <xajouppe@gmail.com>
2015-01-16 19:48:12 +0100

    redirect root

--- monitor_app/monitor_app/urls.py
+++ monitor_app/monitor_app/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'monitor_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(url='/load_charts/')),
     url(r'^load_charts/', include('load_charts.urls')),
 )
